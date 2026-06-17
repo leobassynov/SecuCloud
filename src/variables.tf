2cbb19b5fa5d5b9a94f3f1901472c8faf0ae10b9
leobassynov <leo.bass@ynov.com>
2026-06-17 09:21:22 +0200

    Modify variables in variables.tf
    Updated default values for variables and added SSH key variable.

--- src/variables.tf
+++ src/variables.tf
@@ -1,53 +1,18 @@
 variable "prefix" {
-  description = "Préfixe utilisé pour nommer les ressources"
-  type        = string
-  default     = "ynov1"
+  default = "lbass" # A personnaliser avec votre nom
 }
 
 variable "environment" {
-  description = "Environnement (dev, staging, prod)"
-  type        = string
-  default     = "dev"
+  default = "demo"
 }
 
 variable "project" {
-  description = "Nom du projet"
-  type        = string
-  default     = "tp-azure"
-}
-
-variable "location" {
-  description = "Région Azure"
-  type        = string
-  default     = "swedencentral"
-}
-
-variable "vnet_address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
-}
-
-variable "subnet_address_prefixes" {
-  type    = list(string)
-  default = ["10.0.1.0/24"]
-}
-
-variable "vm_size" {
-  type    = string
-  default = "Standard_B2ts_v2"
-}
-
-variable "admin_username" {
-  type    = string
-  default = "azureuser"
-}
-
-variable "ssh_public_key_path" {
-  type    = string
-  default = "~/.ssh/id_rsa.pub"
+  default = "ynov1"
 }
 
 variable "ssh_key" {
-  description = "Contenu de la clé SSH publique"
-  type        = string
+  description = "Clé publique SSH"
+  type = string
+  sensitive = true
 }
+
